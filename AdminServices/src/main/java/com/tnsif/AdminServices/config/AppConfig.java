22fc2f7c078cb53fa0b5f2f97fc0df4818312800
deiwanthi <deiwanthi@deiwa.lan>
2025-11-07 21:54:01 +0530

    commit

--- AdminServices/src/main/java/com/tnsif/AdminServices/config/AppConfig.java
+++ AdminServices/src/main/java/com/tnsif/AdminServices/config/AppConfig.java
@@ -1,8 +1,25 @@
 package com.tnsif.AdminServices.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@CrossOrigin(origins = "http://localhost:3000")  // React connection
 public class AppConfig {
-	// Reserved for future configuration (CORS, beans, etc.)
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 }
